e9dca615d41aee986eb47692a42ae70b3f3980d2
PrinceMalec <Kallax@hotmail.co.uk>
2015-03-02 17:33:53 +0000

    Tidy Up

--- src/main/java/com/acgmodcrew/kip/proxy/ClientProxy.java
+++ src/main/java/com/acgmodcrew/kip/proxy/ClientProxy.java
@@ -1,9 +1,6 @@
 package com.acgmodcrew.kip.proxy;
 
-import com.acgmodcrew.kip.client.settings.KeyBindings;
 import com.acgmodcrew.kip.reference.Key;
-import com.sun.deploy.util.SessionState;
-import cpw.mods.fml.client.registry.ClientRegistry;
 
 /**
  * Created by Malec on 21/02/2015.
--- src/main/java/com/acgmodcrew/kip/reference/Key.java
+++
@@ -1,9 +0,0 @@
-package com.acgmodcrew.kip.reference;
-
-/**
- * Created by Malec on 26/02/2015.
- */
-public enum Key
-{
-    UNKNOWN, CHARGE, RELEASE
-}
