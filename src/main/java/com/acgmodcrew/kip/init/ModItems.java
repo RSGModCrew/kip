373aa5422c935235860c1556ee18f3c869dcd1da
PrinceMalec <Kallax@hotmail.co.uk>
2015-03-04 22:13:16 +0000

    Fixed Tool localization

--- src/main/java/com/acgmodcrew/kip/init/ModItems.java
+++ src/main/java/com/acgmodcrew/kip/init/ModItems.java
@@ -1,9 +1,6 @@
 package com.acgmodcrew.kip.init;
 
-import com.acgmodcrew.kip.item.ItemKip;
-import com.acgmodcrew.kip.item.ItemKnowledgiumIngot;
-import com.acgmodcrew.kip.item.ItemPaperStack;
-import com.acgmodcrew.kip.item.ItemStupidiumIngot;
+import com.acgmodcrew.kip.item.*;
 import com.acgmodcrew.kip.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -16,12 +13,16 @@ public class ModItems
     public static final ItemKip knowledgium_ingot = new ItemKnowledgiumIngot();
     public static final ItemKip stupidium_ingot = new ItemStupidiumIngot();
     public static final ItemKip paper_stack = new ItemPaperStack();
+    public static final ItemKip grimoire = new ItemGrimoire();
+    public static final ItemKip sum_of_all_knowledge = new ItemSumOfAllKnowledge();
 
     public static void init()
     {
         GameRegistry.registerItem(knowledgium_ingot, "knowledgium_ingot");
         GameRegistry.registerItem(stupidium_ingot, "stupidium_ingot");
         GameRegistry.registerItem(paper_stack, "paper_stack");
+        GameRegistry.registerItem(grimoire, "grimoire");
+        GameRegistry.registerItem(sum_of_all_knowledge, "sum_of_all_knowledge");
 
     }
 
--- src/main/java/com/acgmodcrew/kip/item/ItemModAxe.java
+++ src/main/java/com/acgmodcrew/kip/item/ItemModAxe.java
@@ -3,6 +3,7 @@ package com.acgmodcrew.kip.item;
 import com.acgmodcrew.kip.creativetab.CreativeTabKip;
 import com.acgmodcrew.kip.reference.Reference;
 import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
 
 /**
  * Created by Malec on 03/03/2015.
@@ -16,5 +17,23 @@ public class ItemModAxe extends ItemAxe
         this.setUnlocalizedName(unlocalizedName);
         this.setTextureName(Reference.MOD_ID + ":" + unlocalizedName);
         this.setCreativeTab(CreativeTabKip.KIP_TAB);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack)
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
     }
 }
